d69fb4d697da2fea7960a7d0359db10f8d97c570
Nguyen Dai <trong-dai.nguyen@ant-tech.eu>
2020-04-18 13:54:40 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
-import { AppComponent } from './app.component'
-import { SomethingsComponent } from './somethings/somethings.component'
 import { HttpClientModule } from '@angular/common/http'
+
 import { TranslocoRootModule } from './transloco-root.module'
 import { SharedModule } from './shared/shared.module'
+import { AppComponent } from './app.component'
+import { SomethingsComponent } from './somethings/somethings.component'
 
 @NgModule({
   declarations: [AppComponent, SomethingsComponent],
